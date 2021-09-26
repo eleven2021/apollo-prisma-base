d6b947ce014040ed36d4db7823bc3ee8c0c40dc2
watase <eleven2021>
2021-09-26 11:34:03 +0900

    first

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello GrapQL!',
+    },
+};
+
+async function startApolloServer(){
+    const server = new ApolloServer({
+      typeDefs: typeDefs,
+      resolvers: resolvers
+    })
+    await server.start()
+    const { url } = await server.listen()
+    console.log(`🚀 Server ready at ${url}`);
+}
+
+startApolloServer();
